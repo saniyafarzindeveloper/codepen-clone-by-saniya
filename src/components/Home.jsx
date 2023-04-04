063c7f5967a1286b7571272345603f24a8b92e42
farzinsaniya <saniyafarzin75@gmail.com>
2023-04-04 22:17:04 +0530

    entire working clone

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Code from "./Code";
-
+import Result from "./Result";
 
 
 export default function Home() {
@@ -9,6 +9,7 @@ export default function Home() {
     <>
       <Header />
       <Code />
+      <Result />
     </>
   );
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useState, useEffect, useContext } from "react";
+
+import { DataContext } from "../context/DataProvider";
+
+import { Box, styled } from "@mui/material";
+
+const Container = styled(Box)`
+  height: 43vh;
+`;
 
 export default function Result() {
+  const [src, setSrc] = useState("");
+  const { html, css, js } = useContext(DataContext);
+
+  const srcCode = `
+        <html>
+            <body>${html}</body>
+            <style>${css}</style>
+            <script>${js}</script>
+        </html>
+    `;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrc(srcCode);
+    }, 1000);
+    //unmounting
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
   return (
-    <div>Result</div>
-  )
+    <Container>
+      <iframe
+        srcDoc={srcCode}
+        title="output"
+        sandbox="allow-scripts"
+        width="100%"
+        height="100%"
+      />
+    </Container>
+  );
 }
